2658b47da6574e189cb1b16bc155f8ce6e4330d9
Mark Owen <mark.owen@reactgames.com>
2015-10-22 23:44:50 -0600

    Cleaned up init.el a bit.

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 (require 'package)
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages")
-			 ("melpa" . "https://melpa.milkbox.net/packages/")))
+                         ("marmalade" . "https://marmalade-repo.org/packages")
+                         ("melpa" . "https://melpa.milkbox.net/packages/")))
 
 (defvar my-packages '(better-defaults
                       paredit
@@ -23,20 +23,17 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-(load-theme 'railscasts t nil)
-
-(setq inhibit-startup-message t)
-(defalias 'yes-or-no-p 'y-or-n-p)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
 (require 'magit)
 
-(global-set-key (kbd "C-x g") 'magit-status)
-(setq magit-status-buffer-switch-function 'switch-to-buffer)
-
+(load-theme 'railscasts t nil)
+(setq inhibit-startup-message t)
 (set-face-attribute 'default nil :height 120)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq indent-line-function 'insert-tab)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(global-set-key (kbd "C-x g") 'magit-status)
+(setq magit-status-buffer-switch-function 'switch-to-buffer)
 
 (eshell)
