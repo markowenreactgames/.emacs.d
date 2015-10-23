1e844a980a72d89f9d23eaff7af1003e5efd49bf
Mark Owen <mark.owen@reactgames.com>
2015-10-22 23:36:06 -0600

    Using SSL for package repos.

--- init.el
+++ init.el
@@ -1,8 +1,8 @@
 (require 'package)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("marmalade" . "https://marmalade-repo.org/packages")
+			 ("melpa" . "https://melpa.milkbox.net/packages/")))
 
 (defvar my-packages '(better-defaults
 		      paredit
